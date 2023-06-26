cbb1cf7c5cf3cec2b36da35d18f423e54d702d5f
jejikeh <jejikeh@gmail.com>
2023-06-26 04:08:42 +0300

    Update Injfastructure and Presentation DI, update Auth endpoints and Makefile script

--- src/Versity.Users/External/Presentation/Extensions/IdentityAuthenticationProgramExtension.cs
+++ src/Versity.Users/External/Presentation/Extensions/IdentityAuthenticationProgramExtension.cs
@@ -33,6 +33,7 @@ public static class IdentityAuthenticationProgramExtension
     {
         serviceCollection.AddTransient<IAuthTokenGeneratorService, JwtTokenGeneratorService>();
         serviceCollection.AddTransient<IEmailConfirmMessageService, EmailConfirmMessageGmailService>();
+        serviceCollection.AddTransient<IRefreshTokenGeneratorService, RefreshTokenGeneratorService>();
         
         serviceCollection.AddAuthentication(options =>
         {
