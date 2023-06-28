c76dac656d368b1a4278a5192b2b08ac0aa35003
jejikeh <jejikeh@gmail.com>
2023-06-28 13:38:29 +0300

    format role data seeder

--- src/Versity.Users/External/Infrastructure/Persistence/Configuration/RolesSeederConfiguration.cs
+++ src/Versity.Users/External/Infrastructure/Persistence/Configuration/RolesSeederConfiguration.cs
@@ -9,7 +9,16 @@ public class RolesSeederConfiguration : IEntityTypeConfiguration<IdentityRole>
     public void Configure(EntityTypeBuilder<IdentityRole> builder)
     {
         builder.HasData(
-            new IdentityRole("Admin") { Id = "e56d08b9-8788-4c58-958a-1a7bcb585fc2", NormalizedName = "ADMIN" },
-            new IdentityRole("Member") { Id = "a337651d-2193-4c2d-bfe3-4cccc2ac82fa", NormalizedName = "MEMBER" });
+            new IdentityRole("Admin")
+            {
+                Id = "e56d08b9-8788-4c58-958a-1a7bcb585fc2", 
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole("Member")
+            {
+                Id = "a337651d-2193-4c2d-bfe3-4cccc2ac82fa",
+                NormalizedName = "MEMBER"
+            }
+        );
     }
 }
