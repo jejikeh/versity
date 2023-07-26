d96cfc8ea57438b6a04cfc36dfd8597e7a0b4309
jejikeh <jejikeh@gmail.com>
2023-07-26 11:28:36 +0300

    refactor GrpcUsersServiceTests

--- src/Versity.Users/Tests/Users.Tests/Presentation/GrpcUsersServiceTests.cs
+++ src/Versity.Users/Tests/Users.Tests/Presentation/GrpcUsersServiceTests.cs
@@ -23,42 +23,53 @@ public class GrpcUsersServiceTests
     [Fact]
     public async Task IsUserExist_ShouldReturnTrue_WhenUserExist()
     {
+        // Arrange
         _sender.Setup(x => 
                 x.Send(It.IsAny<IsUserExistQuery>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(true);
-        var request = new GrpcIsUserExistRequest(){ UserId = "test" };
+        
+        var request = new GrpcIsUserExistRequest(){ UserId = Guid.NewGuid().ToString() };
         var service = new GrpcUsersService(_sender.Object);
         
+        // Act
         var response = await service.IsUserExist(request, _serverCallContext.Object);
         
+        // Assert
         response.Should().Be(new GrpcIsUserExistResponse(){ Exist = true });
     }
     
     [Fact]
     public async Task IsUserExist_ShouldReturnFalse_WhenUserNotExist()
     {
+        // Arrange
         _sender.Setup(x => 
                 x.Send(It.IsAny<IsUserExistQuery>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(false);
-        var request = new GrpcIsUserExistRequest(){ UserId = "test" };
+        
+        var request = new GrpcIsUserExistRequest(){ UserId = Guid.NewGuid().ToString() };
         var service = new GrpcUsersService(_sender.Object);
         
+        // Act
         var response = await service.IsUserExist(request, _serverCallContext.Object);
         
+        // Assert
         response.Should().Be(new GrpcIsUserExistResponse(){ Exist = false });
     }
     
     [Fact]
     public async Task GetUserRoles_ShouldReturnRoles_WhenUserExist()
     {
+        // Arrange
         _sender.Setup(x => 
                 x.Send(It.IsAny<GetVersityUserRolesQuery>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(new List<string> { "role1", "role2" });
         var request = new GetUserRolesRequest() { UserId = "test" };
         var service = new GrpcUsersService(_sender.Object);
         
+        // Act
         var response = await service.GetUserRoles(request, _serverCallContext.Object);
         
+        // Assert
         response.Should().Be(new GrpcUserRoles(){ Roles = { "role1", "role2" }});
     }
 }
